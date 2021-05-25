a78c52fc3da0c77ba045a560466ef3b6cefab063
Sriram1662001 <84625931+Sriram1662001@users.noreply.github.com>
2021-05-25 18:35:48 +0530

    Add files via upload

---
+++ DuplicationCount.java
@@ -0,0 +1,31 @@
+package dupilcate;
+import java.util.Scanner;
+import java.util.Arrays;
+class DuplicateCount
+{
+    public static void main(String[] args)
+    {
+    int n,g1=0,g2=0,r=0;
+System.out.println("enter array len:");
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+int A[]=new int[n];
+System.out.println("enter array elements:");
+for(int i=0;i<A.length;i++) {
+A[i]=s.nextInt();
+}
+for(int i=0;i<A.length;i++) {
+if(g1<A[i]){
+g1=A[i];
+}
+
+}
+for(int i=0;i<A.length;i++) {
+if(g1==A[i]){
+  r++;
+}
+}
+System.out.println("first greatest:"+ " "+g1);
+System.out.println("no of times the biggest no repeated:"+" "+r);
+    }
+}
