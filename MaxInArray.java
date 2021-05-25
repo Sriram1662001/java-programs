82b7a3ccb4451557d47dbc2cf3f62829201568e5
Sriram1662001 <84625931+Sriram1662001@users.noreply.github.com>
2021-05-25 17:50:20 +0530

    Update MaxInArray.java

--- MaxInArray.java
+++ MaxInArray.java
@@ -4,8 +4,8 @@ class MaxInArray
 {
     public static void main(String[] args)
     {
-    int n,great1=0,great2=0;
-    System.out.println("enter array length:");
+    int n,g1=0,g2=0;
+    System.out.println("enter array len:");
     Scanner s=new Scanner(System.in);
     n=s.nextInt();
     int A[]=new int[n];
@@ -14,17 +14,17 @@ class MaxInArray
     A[i]=s.nextInt();
     }
     for(int i=0;i<A.length;i++) {
-    if(great1<A[i]){
-    great1=A[i];
+    if(g1<A[i]){
+    g1=A[i];
     }
    
     }
     for(int i=0;i<A.length;i++) {
-  if(great2<A[i] && great1>A[i]){
-      great2=A[i];
+  if(g2<A[i] && g1>A[i]){
+      g2=A[i];
   }
   }
-    System.out.println("first high:"+great1);
-    System.out.println("second high:"+great2);
+    System.out.println("first high:"+g1);
+    System.out.println("second high:"+g2);
     }
 }
